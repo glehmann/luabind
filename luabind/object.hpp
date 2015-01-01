f54a1d1cd48e480848b0fa829049f0abd6a92afd
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-01-01 07:49:11 +0100

    boost 1.57 compatibility

--- luabind/object.hpp
+++ luabind/object.hpp
@@ -557,6 +557,7 @@ namespace detail
       handle m_key;
   };
 
+#if BOOST_VERSION < 105700
 // Needed because of some strange ADL issues.
 
 #define LUABIND_OPERATOR_ADL_WKND(op) \
@@ -578,6 +579,8 @@ namespace detail
   LUABIND_OPERATOR_ADL_WKND(!=)
 
 #undef LUABIND_OPERATOR_ADL_WKND
+
+#endif // BOOST_VERSION < 105700
  
 } // namespace detail
 
